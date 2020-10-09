03bf73074252216aa075cffbae4df94afbba3a9b
Abdurahman Ahmed <yoabdurahman@hotmail.com>
2020-10-08 20:40:20 -0400

    second question complete

--- LAB-TEST-1/question-2/callbacks.js
+++ LAB-TEST-1/question-2/callbacks.js
@@ -16,12 +16,28 @@ const delayedException = () => {
 }
 
 
-function rejectedPromise(){
-    return new Promise((resolve,reject) => {
-        
+function resolvedPromise(){
+    return new Promise((resolve) => {
+        let success = {'message': 'delayed success!'};
+        resolve(console.log(success));
+        setTimeout(resolve, 500);
     })
 }
 
 
+function rejectedPromise(){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            try {
+                throw new Error('error! delayed exception!');
+            } catch (e) {
+                console.error(e);
+            }
+        }, 500);
+    })
+}
+
 delayedSuccess()
 delayedException()
+resolvedPromise()
+rejectedPromise()
