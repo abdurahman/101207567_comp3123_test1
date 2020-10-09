176146fefd9a42917b3edbe677c427c69b64522b
Abdurahman Ahmed <yoabdurahman@hotmail.com>
2020-10-08 20:30:15 -0400

    working on question 2

---
+++ LAB-TEST-1/question-2/callbacks.js
@@ -0,0 +1,27 @@
+const delayedSuccess = () => {
+    setTimeout(() => {
+        let success = {'message': 'delayed success!'}
+        console.log(success);
+    }, 500)
+}
+
+const delayedException = () => {
+    setTimeout(() => {
+        try {
+            throw new Error('error! delayed exception!');
+        } catch (e) {
+            console.error(e);
+        }
+    }, 500);
+}
+
+
+function rejectedPromise(){
+    return new Promise((resolve,reject) => {
+        
+    })
+}
+
+
+delayedSuccess()
+delayedException()
