70c9812c12f1b885da9bfdb955e812db5f91d785
Abdurahman Ahmed <yoabdurahman@hotmail.com>
2020-10-08 21:00:43 -0400

    question 3 incomplete (test done)

--- LAB-TEST-1/question-3/logmodifier.js
+++ LAB-TEST-1/question-3/logmodifier.js
@@ -3,12 +3,16 @@ const path = require('path');
 
 const dir = require('logs');
 
+// removes log files
 fs.readdir(dir, (err, files) => {
     if (err) throw err;
     
     for(const file of files){
+        console.log('removing files... ' + file)
         fs.unlink(path.join(dir, files), err => {
             if (err) throw err;
         });
     }
 });
+
+// adds log files (wasnt able to complete in time)
